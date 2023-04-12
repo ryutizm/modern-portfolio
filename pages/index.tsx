2d743756142b7ece6d4f5a0f44cbcf12d8a5f73d
ryutizm <ryutizm1103@gmail.com>
2023-04-12 18:22:44 +0900

    foo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
-import SimpleSlider from "@/components/Slider";
 
 
 const Home: NextPage = () => {
